6097026dea3bdfb12fccf8882aec4af6bffa6abf
Darren Ross <darren.ross@bellevuecollege.edu>
2020-01-27 19:44:17 -0800

    More stuff.

--- deck.js
+++ deck.js
@@ -1,6 +1,15 @@
 let Deck = function(){
 
   let cardArray = [];
-  
 
+  const suits = [1,2,3,4];
+  const ranks = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+  for (let suit in suits){
+    for (let rank in ranks){
+      let card = {Suit: suits[suit], Rank: ranks[rank]};
+      this.cardArray.push(card);
+    }
+  }
+return cardArray;
 }
---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  let playerArray = [];
+  let computerArray = [];
+
+  document.getElementById("deal_hands").addEventListener("click", function () {
+    Deck.Load();
+    
+  });
+
+});
